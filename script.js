5de940e071916149c9e9814d53b474135503881c
AbrahamGalvezV <2abrahamtattoosk8@gmail.com>
2026-01-23 13:34:58 +0100

    create filter

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const botones = document.querySelectorAll(".button-apply-job");
 
-const botones = document.querySelectorAll('.button-apply-job')
+  botones.forEach((boton) => {
+    boton.addEventListener("click", () => {
+      boton.textContent = "¡Aplicado!";
+      boton.classList.add("is-applied");
+      boton.disabled = true;
+    });
+  });
 
-botones.forEach(boton => {
-	boton.addEventListener('click', () => {
-	  boton.textContent = '¡Aplicado!'
-    boton.classList.add('is-applied')
-	  boton.disabled = true
-	})
-})
+  const filterLocation = document.querySelector("#filter-location");
+  const filterTechnology = document.querySelector("#filter-technology");
+  const filterExperience = document.querySelector("#experience-level");
 
- const filter = document.querySelector('#filter-location')
+  const mensaje = document.querySelector("#filter-selected-value");
+  const jobs = document.querySelectorAll(".job-listing-card");
 
- filter.addEventListener('change', () => {
-    console.log(filter.value)
- })
+  function filterJobs() {
+    const locationValue = filterLocation.value;
+    const technologyValue = filterTechnology.value;
+    const experienceValue = filterExperience.value;
 
+    mensaje.textContent = `
+    ${technologyValue || "Todas las tecnologías"} · 
+    ${locationValue || "Todas las ubicaciones"} · 
+    ${experienceValue || "Todos los niveles"}
+  `;
 
+    jobs.forEach((job) => {
+      const jobLocation = job.dataset.modalidad;
+      const jobTechnology = job.dataset.technology;
+      const jobExperience = job.dataset.experience;
 
+      const matchLocation = !locationValue || locationValue === jobLocation;
 
+      const matchTechnology =
+        !technologyValue || technologyValue === jobTechnology;
 
-    
+      const matchExperience =
+        !experienceValue || experienceValue === jobExperience;
 
+      job.style.display =
+        matchLocation && matchTechnology && matchExperience ? "flex" : "none";
+    });
+  }
 
+  filterLocation.addEventListener("change", filterJobs);
+  filterTechnology.addEventListener("change", filterJobs);
+  filterExperience.addEventListener("change", filterJobs);
+});
 
 
