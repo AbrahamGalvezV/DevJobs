0e5db396ffd79c0c592293840f28568e6236225a
AbrahamGalvezV <2abrahamtattoosk8@gmail.com>
2026-02-05 12:24:35 +0100

    Modificate and create function fom App.jsx, JobCard.jsx, JobListingCard.jsx

--- 03-react/src/App.jsx
+++ 03-react/src/App.jsx
@@ -1,17 +1,24 @@
 import { Header } from "./components/Header/Header";
-import { JobCard } from "./components/JobCard/JobCard";
 import { Search } from "./components/Search/Search";
 import { Pagination } from "./components/Pagination/Pagination";
 import { Footer } from "./components/Footer/Footer";
 import { useState } from "react";
+import { JobListings } from "./components/JobListingCard/JobListingCard";
+import jobsData from './data.json'
+
+const RESULTS_PER_PAGE = 5
 
 function App() {
   const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = 5;
+  const totalPages = Math.ceil(jobsData.length / RESULTS_PER_PAGE);
+
+  const pagedResults = jobsData.slice(
+    (currentPage - 1) * RESULTS_PER_PAGE, // Página 1 -> 0, página 2 -> 5, página 3 -> 10
+    currentPage * RESULTS_PER_PAGE // Página 1 -> 5, página 1 -> 10, página 2 -> 15
+  )
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
-    console.log(page);
   }
 
   return (
@@ -19,7 +26,8 @@ function App() {
       <Header />
       <main>
         <Search />
-        <JobCard currentPage={currentPage}/>
+        {/* <JobCard currentPage={currentPage}/> */}
+        <JobListings jobs={pagedResults} />
         <Pagination 
         currentPage={currentPage} 
         totalPages={totalPages} 
--- 03-react/src/components/JobCard/JobCard.jsx
+++ 03-react/src/components/JobCard/JobCard.jsx
@@ -1,40 +1,77 @@
-import data from "../../data.json";
-import { useState } from "react";
-import "./JobCard.css"
+// import data from "../../data.json";
+// import { useState } from "react";
+// import "./JobCard.css"
 
-export function JobCard() {
+import { useState } from "react"
+
+// export function JobCard() {
+//   return (
+//     <section className="job-listings">
+//       <h2>Resultados de busqueda</h2>
+
+//       {data.map((job, index) => (
+//         <JobItem key={index} job={job} />
+//       ))}
+//     </section>
+//   );
+// }
+
+// function JobItem({ job }) {
+//   const [isApplied, setIsApplied] = useState(false);
+
+//   const { titulo, empresa, ubicacion, descripcion } = job;
+
+//   return (
+//     <article className="job-listing-card">
+//       <div>
+//         <h3>{titulo}</h3>
+//         <small>
+//           {empresa} - {ubicacion} - ¿He aplicado? {isApplied ? "Sí" : "No"}
+//         </small>
+//         <p>{descripcion}</p>
+//       </div>
+
+//       <button
+//         className={`button-apply-job ${isApplied ? "is-applied" : ""}`}
+//         onClick={() => setIsApplied(!isApplied)}
+//       >
+//         {isApplied ? "Aplicado" : "Aplicar"}
+//       </button>
+//     </article>
+//   );
+// }
+
+
+
+export function JobCard ({ job }) {
+    const [isApplied, setIsApplied] = useState(false)
+  
+    const handleApplyClick = () => {
+      setIsApplied(true)
+    }
+
+    const buttonClasses = isApplied ? 'button-apply-job is-applied' : 'button-apply-job'
+    const buttonText = isApplied ? '¡Aplicado!' : 'Aplicar'
+
   return (
-    <section className="job-listings">
-      <h2>Resultados de busqueda</h2>
-
-      {data.map((job, index) => (
-        <JobItem key={index} job={job} />
-      ))}
-    </section>
-  );
-}
-
-function JobItem({ job }) {
-  const [isApplied, setIsApplied] = useState(false);
-
-  const { titulo, empresa, ubicacion, descripcion } = job;
-
-  return (
-    <article className="job-listing-card">
-      <div>
-        <h3>{titulo}</h3>
-        <small>
-          {empresa} - {ubicacion} - ¿He aplicado? {isApplied ? "Sí" : "No"}
-        </small>
-        <p>{descripcion}</p>
-      </div>
-
-      <button
-        className={`button-apply-job ${isApplied ? "is-applied" : ""}`}
-        onClick={() => setIsApplied(!isApplied)}
+    <article 
+      className="job-listing-card"
+      data-modalidad={job.data.modalidad}
+      data-nivel={job.data.nivel}
+      data-technology={job.data.technology}
       >
-        {isApplied ? "Aplicado" : "Aplicar"}
-      </button>
+        <div>
+          <h3>{job.titulo}</h3>
+          <small>{job.empresa} | {job.ubicacion}</small>
+          <p>{job.descripcion}</p>
+        </div>
+        <button className={buttonClasses} onClick={handleApplyClick}>{buttonText}</button>
     </article>
-  );
+  )
 }
+
+
+
+
+
+
