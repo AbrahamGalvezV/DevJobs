12c1d629076779712160a793f95b05616de99442
AbrahamGalvezV <2abrahamtattoosk8@gmail.com>
2026-04-08 20:04:39 +0200

    Update in useFilters.jsx, include useSearchParams

--- 04-router-and-zustand/src/components/Hooks/useFilter.jsx
+++ 04-router-and-zustand/src/components/Hooks/useFilter.jsx
@@ -1,33 +1,30 @@
 import { useEffect, useState } from "react";
-import { useRouter } from "./useRouter";
+import { useSearchParams } from "react-router";
 
 const RESULTS_PER_PAGE = 4;
 
 export const useFilters = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [filters, setFilters] = useState(() => {
-    const params = new URLSearchParams(window.location.search);
     return {
-      technology: params.get("technology") || "",
-      location: params.get("location") || "",
-      experienceLevel: params.get("experienceLevel") || "",
+      technology: searchParams.get("technology") || "",
+      location: searchParams.get("location") || "",
+      experienceLevel: searchParams.get("experienceLevel") || "",
     };
   });
 
-  const [textToFilter, setTextToFilter] = useState(() => {
-    const params = new URLSearchParams(window.location.search);
-    return params.get("text") || "";
-  });
+  const [textToFilter, setTextToFilter] = useState(
+    () => searchParams.get("text") || "",
+  );
 
   const [currentPage, setCurrentPage] = useState(() => {
-    const params = new URLSearchParams(window.location.search);
-    const page = Number(params.get("page"));
+    const page = Number(searchParams.get("page"));
     return Number.isNaN(page) || page < 1 ? 1 : page;
   });
 
   const [jobs, setJobs] = useState([]);
   const [total, setTotal] = useState(0);
   const [loading, setLoading] = useState(true);
-  const { navigateTo } = useRouter();
 
   const handleClearFilter = () => {
     setFilters({
@@ -77,22 +74,17 @@ export const useFilters = () => {
   }, [filters, textToFilter, currentPage]);
 
   useEffect(() => {
-    const params = new URLSearchParams();
+    setSearchParams((params) => {
+      if (textToFilter) params.set("text", textToFilter);
+      if (filters.technology) params.set("technology", filters.technology);
+      if (filters.location) params.set("type", filters.location);
+      if (filters.experienceLevel)params.set("level", filters.experienceLevel);
+      if (currentPage > 1) params.set("page", currentPage);
+  
+      return params
+    })
 
-    if (textToFilter) params.append("text", textToFilter);
-    if (filters.technology) params.append("technology", filters.technology);
-    if (filters.location) params.append("type", filters.location);
-    if (filters.experienceLevel) params.append("level", filters.experienceLevel);
-    if (currentPage > 1) params.append("page", currentPage);
-
-    const newUrl = params.toString()
-      ? `${window.location.pathname}?${params.toString()}`
-      : window.location.pathname;
-
-    if (newUrl !== window.location.pathname + window.location.search) {
-      navigateTo(newUrl);
-    }
-  }, [filters, textToFilter, currentPage, navigateTo]);
+  }, [filters, textToFilter, currentPage, setSearchParams]);
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
